f8259274c05d1a085bf6822a33918693193fbb56
8mamo10 <562105+8mamo10@users.noreply.github.com>
2020-01-26 16:44:29 +0000

    Enable to use c++

--- mod_mrhc.cpp
+++ mod_mrhc.cpp
@@ -42,6 +42,10 @@
 #include "http_protocol.h"
 #include "ap_config.h"
 
+#include <string>
+
+extern "C" module AP_MODULE_DECLARE_DATA mrhc_module;
+
 /* The sample content handler */
 static int mrhc_handler(request_rec *r)
 {
@@ -67,6 +71,10 @@ static int mrhc_handler(request_rec *r)
         ap_rputs("<br/>", r);
         sprintf(str, "password: %s", password);
         ap_rputs(str, r);
+        ap_rputs("<br/>", r);
+        std::string hoge = "test string";
+        ap_rputs(hoge.c_str(), r);
+        ap_rputs("<br/>", r);
         return OK;
     }
     ap_rputs("not reach here\n", r);
@@ -78,14 +86,15 @@ static void mrhc_register_hooks(apr_pool_t *p)
     ap_hook_handler(mrhc_handler, NULL, NULL, APR_HOOK_MIDDLE);
 }
 
-/* Dispatch list for API hooks */
-module AP_MODULE_DECLARE_DATA mrhc_module = {
-    STANDARD20_MODULE_STUFF,
-    NULL,                  /* create per-dir    config structures */
-    NULL,                  /* merge  per-dir    config structures */
-    NULL,                  /* create per-server config structures */
-    NULL,                  /* merge  per-server config structures */
-    NULL,                  /* table of config file commands       */
-    mrhc_register_hooks  /* register hooks                      */
+extern "C" {
+    /* Dispatch list for API hooks */
+    module AP_MODULE_DECLARE_DATA mrhc_module = {
+        STANDARD20_MODULE_STUFF,
+        NULL,                  /* create per-dir    config structures */
+        NULL,                  /* merge  per-dir    config structures */
+        NULL,                  /* create per-server config structures */
+        NULL,                  /* merge  per-server config structures */
+        NULL,                  /* table of config file commands       */
+        mrhc_register_hooks  /* register hooks                      */
+    };
 };
-
