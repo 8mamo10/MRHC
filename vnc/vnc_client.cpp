c575f9b465b653cd8ddee0e1b0fecb6e24712709
8mamo10 <562105+8mamo10@users.noreply.github.com>
2020-02-01 23:55:29 +0000

    Add vnc client

---
+++ vnc/vnc_client.cpp
@@ -0,0 +1,11 @@
+#include "vnc_client.h"
+
+VncClient::VncClient()
+{
+    value = 100;
+}
+
+int VncClient::getValue()
+{
+    return value;
+}
---
+++ vnc/vnc_client.h
@@ -0,0 +1,13 @@
+#ifndef __VNC_CLIENT_H__
+#define __VNC_CLIENT_H__
+
+class VncClient
+{
+ private:
+    int value;
+ public:
+    VncClient();
+    int getValue();
+};
+
+#endif
