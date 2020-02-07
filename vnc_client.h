9aebfadc1a797d72231d89680f86efb88e0eadad
8mamo10 <562105+8mamo10@users.noreply.github.com>
2020-02-07 23:51:17 +0000

    Add function to parse vnc connection info from basic auth input

--- vnc_client.h
+++ vnc_client.h
@@ -8,6 +8,9 @@ class VncClient
  private:
     std::string host;
     std::string password;
+
+    std::string parseHost();
+    int parsePort();
  public:
     VncClient();
     void setConnectionInfo(std::string host, std::string password);
