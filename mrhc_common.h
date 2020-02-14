ecfaba77b4eeb2b5ef54377be3c2a4a9a4f74db8
8mamo10 <562105+8mamo10@users.noreply.github.com>
2020-02-14 12:11:17 +0000

    Write file name and line number to logs by dirty way

--- mrhc_common.h
+++ mrhc_common.h
@@ -9,6 +9,8 @@
 #include "http_log.h"
 #include "http_protocol.h"
 
+using namespace std;
+
 #define BUF_SIZE 1024
 // I'm not sure hot to know the index of the module.
 // I saw the httpd.conf and count the order of `LoadModule mrhc_module`
@@ -18,11 +20,9 @@
 /*     cerr << __FILE__ << ":" << __LINE__ << " --- " << msg << endl; */
 
 #define log_access(msg) \
-    apr_table_set(r->notes, "mrhc_log", msg);
+    apr_table_set(r->notes, "mrhc_log", ("[" + string(__FILE__) + ":" + to_string(__LINE__) + "] " + string(msg)).c_str());
 
 #define log_error(msg) \
-    ap_log_rerror(__FILE__, __LINE__, MODULE_INDEX, APLOG_NOTICE, OK, r, msg);
-
-using namespace std;
+    ap_log_rerror(__FILE__, __LINE__, MODULE_INDEX, APLOG_NOTICE, OK, r, "[%s:%d] %s", __FILE__, __LINE__, msg);
 
 #endif
