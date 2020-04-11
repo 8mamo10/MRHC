0e724ea7b4820801158787738fb60ebefe61afda
8mamo10 <562105+8mamo10@users.noreply.github.com>
2020-04-11 20:59:22 +0000

    Add setup script for dev env

---
+++ script/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install apache2-dev 
+sudo apt install libopencv-dev
+sudo apt install cmake
+sudo apt install libgtest-dev
+cd /usr/src/gtest/
+sudo cmake CMakeLists.txt
+sudo make
+sudo cp *.a /usr/lib
