eecddc27577601e2ca97e05ebaa63cbe81ba565e
8mamo10 <562105+8mamo10@users.noreply.github.com>
2020-04-16 00:04:31 +0000

    Fix path for test scripts

--- test/start_vnc.sh
+++ test/start_vnc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 vncserver :1 -rfbport 6624
-perl fake_vnc_server_rfb_3_3.pl &
-perl fake_vnc_server_rfb_3_8.pl &
+perl ./test/fake_vnc_server_rfb_3_3.pl &
+perl ./test/fake_vnc_server_rfb_3_8.pl &
