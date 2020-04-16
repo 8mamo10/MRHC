d147a1a428952fc8e33a35a8eb81a9ad85ea0b2d
8mamo10 <562105+8mamo10@users.noreply.github.com>
2020-04-16 18:35:18 +0000

    Add wait after starting vncserver

--- test/start_vnc.sh
+++ test/start_vnc.sh
@@ -3,6 +3,7 @@ expect -c "
 spawn vncserver :1 -rfbport 6624
 expect \"Password:\"
 send -- \"testtest\n\"
+expect
 "
 perl ./test/fake_vnc_server_rfb_3_3.pl &
 perl ./test/fake_vnc_server_rfb_3_8.pl &
