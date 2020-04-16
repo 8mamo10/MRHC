4852b60b91ceb3d7e397e9f1e919d97821907840
8mamo10 <562105+8mamo10@users.noreply.github.com>
2020-04-16 19:14:49 +0000

    Update CI

--- test/start_vnc.sh
+++ test/start_vnc.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 expect -c "
 spawn vncserver :1 -rfbport 6624
-expect \"You will require a password to access your desktops.\n\"
-expect \"Password:\"
-send \"testtest\n\"
 expect
 "
 perl ./test/fake_vnc_server_rfb_3_3.pl &
