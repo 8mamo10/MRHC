a331d8c59373733cc6cb7d85e63b9195558cf6c7
8mamo10 <562105+8mamo10@users.noreply.github.com>
2020-04-17 18:15:58 +0000

    Update log dir

--- logger.cpp
+++ logger.cpp
@@ -1,6 +1,7 @@
 #include "logger.h"
 
-const std::string logger::filename = "/usr/local/apache2/logs/mrhc.log";
+//const std::string logger::filename = "/usr/local/apache2/logs/mrhc.log";
+const std::string logger::filename = "/var/log/apache2/mrhc.log";
 logger* logger::instance = NULL;
 std::ofstream logger::logfile;
 
