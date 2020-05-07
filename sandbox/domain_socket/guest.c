0e5d6af92df2132db461a439d290c03faffbf602
8mamo10 <562105+8mamo10@users.noreply.github.com>
2020-05-07 23:13:23 +0000

    Pass the connected sockfd via domain socket and share among multi processes

--- sandbox/domain_socket/guest.c
+++ sandbox/domain_socket/guest.c
@@ -1,11 +1,48 @@
 #include "fdtransport.h"
 #include <stdio.h>
 
+#include "tmp_vnc_client.h"
+#include <string>
+
+using namespace std;
+
 int main(void)
 {
     int gate = connect_gate("./local.sock");
     pid_t msg = getpid();  /* pidをメッセージとして送る */
-    sendfd(gate, 1, &msg, sizeof(msg));
+
+    // set up vnc client
+    vnc_client *v = new vnc_client();
+    string localhost = "127.0.0.1";
+    v->set_host(localhost);
+    string host = v->get_host();
+    cout << "host: " << host << endl;
+
+    v->set_host_c(localhost.c_str(), localhost.length());
+    char host_c[BUF_SIZE] = {};
+    v->get_host_c(host_c);
+    cout << "host_c: " << host_c << endl;
+
+    v->set_port(6623);
+    int port = v->get_port();
+    cout << "port: " << port << endl;
+
+    if (!v->connect_to_server()) {
+        cout << "failed to connect_to_server" << endl;
+        delete v;
+        return -1;
+    }
+    cout << "sockfd: " << v->get_sockfd() << endl;
+
+    if (!v->recv_protocol_version()) {
+        cout << "failed to recv_protool_version" << endl;
+        delete v;
+        return -1;
+    }
+
+    //
+    //sendfd(gate, 1, &msg, sizeof(msg));
+    sendfd(gate, v->get_sockfd(), &msg, sizeof(msg));
     perror("end");
     return 0;
 }
--- sandbox/domain_socket/host.c
+++ sandbox/domain_socket/host.c
@@ -3,6 +3,11 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 int main(void)
 {
     int gate = listen_gate("./local.sock");
@@ -12,23 +17,30 @@ int main(void)
 
     /* 標準入力 → 受け取ったファイルディスクリプタ */
     /* 正しいwrite(2)の使い方：http://kzk9.net/column/write.html */
-    char buf[BUFSIZ];
-    ssize_t len;
-    while( (len = read(0, buf, sizeof(buf))) > 0 ) {
-        const char *p = buf;
-        const char * const endp = p + len;
-        do {
-            const ssize_t nbytes = write(fd, p, endp - p);
-            if( nbytes < 0 ) {
-                if( errno == EINTR || errno == EAGAIN ) {
-                    continue;
-                } else {
-                    goto end;
-                }
-            }
-            p += nbytes;
-        } while (p < endp);
+    /* char buf[BUFSIZ]; */
+    /* ssize_t len; */
+    /* while( (len = read(0, buf, sizeof(buf))) > 0 ) { */
+    /*     const char *p = buf; */
+    /*     const char * const endp = p + len; */
+    /*     do { */
+    /*         const ssize_t nbytes = write(fd, p, endp - p); */
+    /*         if( nbytes < 0 ) { */
+    /*             if( errno == EINTR || errno == EAGAIN ) { */
+    /*                 continue; */
+    /*             } else { */
+    /*                 goto end; */
+    /*             } */
+    /*         } */
+    /*         p += nbytes; */
+    /*     } while (p < endp); */
+    /* } */
+    // RFB 003.003\n
+    const char RFB_PROTOCOL_VERSION_3_3[] = {0x52, 0x46, 0x42, 0x20, 0x30, 0x30, 0x33, 0x2e, 0x30, 0x30, 0x33, 0x0a};
+    int length = send(fd, &RFB_PROTOCOL_VERSION_3_3, 12, 0);
+    if (length < 0) {
+        goto end;
     }
+    std::cout << "send: " << length << std::endl;
  end:
     perror("end");
     unlink("./local.sock");
